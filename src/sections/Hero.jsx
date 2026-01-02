46ff558405390b00a1841d36862705447a43d739
Mohsina Alima <mohsinaalima2006@gmail.com>
2026-01-02 16:35:27 +0530

    add changes in hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,5 @@
+import { words } from "../constants/index.js";
+
 const Hero = () => {
   return (
     <section id='hero' className='relative overflow-hidden'>
@@ -16,24 +18,25 @@ const Hero = () => {
           <div className='flex flex-col gap-7'>
             <div className='hero-text'>
               <h1>
-                Shaping 
-                <span className="slide">
-                    <span className="wrapper">
-                        {words.map((word) => (
-                            <span key={word.text} className="flex items-center md:gap-3 gap-1 pb-2">
-                                <img src={word.imgPath} alt={word.text} 
-                                className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"/>
+                Shaping
+                <span className='slide'>
+                  <span className='wrapper'>
+                    {words.map((word) => (
+                      <span
+                        key={word.text}
+                        className='flex items-center md:gap-3 gap-1 pb-2'
+                      >
+                        <img
+                          src={word.imgPath}
+                          alt={word.text}
+                          className='xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50'
+                        />
 
-                                <span>{word.text}</span>
-
-                            </span>
-                            
-
-                        ))}
-                    </span>
-
+                        <span>{word.text}</span>
+                      </span>
+                    ))}
+                  </span>
                 </span>
-
               </h1>
               <h1>into Real Projects</h1>
               <h1>that Deliver Results</h1>
