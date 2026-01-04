c861533a02e816c4276ace61a67373cd3abe5a11
Mohsina Alima <mohsinaalima2006@gmail.com>
2026-01-05 00:29:48 +0530

    public/models

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import Hero from "./sections/Hero.jsx";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
 
-const App = () => {
+function Room() {
+  const { scene } = useGLTF("/models/optimized-room.glb");
+  return <primitive object={scene} scale={2.5} />;
+}
+
+export default function App() {
   return (
-    <main className='relative'>
-      <Hero />
-    </main>
+    <Canvas
+      camera={{ position: [0, 6, 12] }}
+      style={{ height: "100vh", width: "100vw" }}
+    >
+      <ambientLight intensity={2} />
+      <directionalLight position={[5, 5, 5]} />
+      <Room />
+      <OrbitControls />
+    </Canvas>
   );
-};
-
-export default App;
+}
